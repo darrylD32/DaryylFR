23434bd20eadaed4848e1b4b04d406c1121771d0
darrylD32 <matthewj32323@gmail.com>
2025-01-29 16:12:14 -0600

    Update .py

--- .py
+++ .py
@@ -1,17 +1,1 @@
-from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/spin', methods=['POST'])
-def spin():
-    import random
-    wheel = ["red"] * 7 + ["black"] * 7 + ["green"] * 1
-    result = random.choice(wheel)
-    return jsonify({'result': result})
-
-if __name__ == '__main__':
-    app.run(debug=True)
